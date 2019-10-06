41b7996e8d8dab2f65fd2905067f532831f34909
IvanaDjikic <ivdjikic@gmail.com>
2019-10-06 21:20:08 +0200

    spiceWorld change

--- src/exercises/_1_SpiceWorld.java
+++ src/exercises/_1_SpiceWorld.java
@@ -1,23 +1,29 @@
 package exercises;
 
+import java.util.function.Consumer;
+
 public class _1_SpiceWorld {
 	/*
 	 * Replace the anonymous inner class with a lambda expression.
 	 */
 
+	static Consumer<Integer> performMethod = (x) -> System.out.println("Do backflips");
+
 	public static void main(String[] args) {
-		SpiceGirl sporty = new SpiceGirl() {
-			
-			@Override
-			public void perform() {
-				System.out.println("Do backflips.");
-				
-			}
-		};
+		SpiceGirl sporty = new SpiceGirl();
+		sporty.setPerformingMethod(performMethod);
 		sporty.perform();
 	}
 }
 
-interface SpiceGirl {
-	void perform();
+class SpiceGirl {
+	private Consumer<Integer> performingMethod;
+
+	void setPerformingMethod(Consumer<Integer> performingMethod) {
+		this.performingMethod = performingMethod;
+	}
+
+	void perform() {
+		performingMethod.accept(0);
+	}
 }
